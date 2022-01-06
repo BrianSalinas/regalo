6e9ea46a5e73f0ac10f82e2094a91d8da43662aa
Brian Salinas <briansalinas26@gmail.com>
2022-01-06 10:00:05 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GaleriaComponent } from './galeria/galeria.component';
+import { LibroComponent } from './libro/libro.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "main",
+    component: MainComponent
+  },
+  {
+    path: "libro",
+    component: LibroComponent
+  },
+  {
+    path: "galeria",
+    component: GaleriaComponent
+  },
+  {
+    path: "**",
+    pathMatch: "full",
+    redirectTo: "main"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GaleriaComponent } from './galeria/galeria.component';
+import { LibroComponent } from './libro/libro.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GaleriaComponent,
+    LibroComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
